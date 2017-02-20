10233c696f944e20cd70c838926bfb72dcffe3d0
Christie <chriziegler@berkeley.edu>
2017-02-20 14:15:49 -0600

    create bunny object

--- starter-code.py
+++ starter-code.py
@@ -1,5 +1,7 @@
 rooms = [{"name":"entranceway", "msg":"","egg":True,"basket":False},
          {""}}]
+bunny = {"location":0,"basket":False,"eggs":0}
+gameover = False
 def msg(room):
 	if room['msg'] == '': #There is no custom message
 		return 'You have entered the ' + room['name']
@@ -19,7 +21,13 @@ def get_choice(room,dir):
 		return -1
 
 	if room['directions'][choice] == 0:
-		return 4
+		return 0
 
 	else:
 		return choice
+def statusupdate():
+
+
+def main():
+	while(not gameover):
+		userinput = input("Pick a direction. Type 'HELP' for help.")
